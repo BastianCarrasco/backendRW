525b2a94519b65c8f88ae09f64889279d65aa9ea
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-05-28 21:24:19 -0400

    CRUD links

--- index.js
+++ index.js
@@ -136,6 +136,51 @@ app.delete('/fondos/:id', async (req, res) => {
   }
 });
 
+// Obtener todos los postualciones
+app.get('/postulaciones', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM postulaciones ORDER BY id');
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error al obtener fondos' });
+  }
+});
+
+app.post('/postulaciones', async (req, res) => {
+  const { link } = req.body;
+  
+  try {
+    const { rows } = await pool.query(
+      'INSERT INTO postulaciones(link) VALUES($1) RETURNING *',
+      [link]
+    );
+    res.status(201).json(rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error al crear postulación' });
+  }
+});
+
+app.delete('/postulaciones/:id', async (req, res) => {
+  const { id } = req.params;
+  
+  try {
+    const { rowCount } = await pool.query('DELETE FROM postulaciones WHERE id = $1', [id]);
+    
+    if (rowCount === 0) {
+      return res.status(404).json({ error: 'Postulación no encontrada' });
+    }
+    
+    res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error al eliminar postulación' });
+  }
+});
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor listo en http://localhost:${PORT}`);
